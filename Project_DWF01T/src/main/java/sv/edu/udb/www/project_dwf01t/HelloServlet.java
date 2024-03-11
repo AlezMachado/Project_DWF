d34c378b8fad5d9a3982cc1048eb848c5b3a3d0d
ZANK123-F <frankmarroquin.dev@gmail.com>
2024-03-10 22:51:52 -0600

    subiendo primeros cambios

--- Project_DWF01T/src/main/java/sv/edu/udb/www/project_dwf01t/HelloServlet.java
+++ Project_DWF01T/src/main/java/sv/edu/udb/www/project_dwf01t/HelloServlet.java
@@ -10,7 +10,7 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello World!";
+        message = "hola gente probamdo a git!";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
